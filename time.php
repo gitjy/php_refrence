1e37deb606039c069525f6f2a585958c7ef501e3
siyu <jy1107985549@163.com>
2022-05-17 17:32:57 +0800

    commit

--- time.php
+++ time.php
@@ -16,6 +16,7 @@ echo "<br/>+2 days today: " . date('c', strtotime('+2 days today'));
 echo "<br/>today +2 days " . date('c', strtotime('today +2 days'));
 echo "<br/>Y-M-01: " . date('Y-M-01');
 echo "<br/>Y-m-01: " . date('c', strtotime(date('Y-M-01')));
+echo "<br/>now:". date('c', strtotime('now')), "</br>";
 echo "<br/>noon: " . date('c', strtotime('noon'));
 echo "<br/>tomorrow: " . date('c', strtotime('tomorrow'));
 echo "<br/>yestday: " . date('c', strtotime('yesterday'));
@@ -27,6 +28,7 @@ echo "<br/>midnight: " . date('c', strtotime('midnight'));
 echo "<br/>00:00: " . date('c', strtotime('00:00'));
 echo "<br/>0.0: " . date('c', strtotime('0.0'));
 echo "<br/>某天2021-11-05的开始: " . date('c', strtotime('2021-11-05 midnight'));
+echo "<br/>2015-01-02 +1 day:". date('c', strtotime('2015-01-02 +1 day')), "</br>";
 echo "<br/>23:59:59: " . date('c', strtotime('23:59:59'));
 echo "<br/>15:40:59: " . date('c', strtotime('15:40:59'));
 echo "<br/>today -2 days: " . date('c', strtotime('today -2 days'));
@@ -46,11 +48,15 @@ echo "<br/>date('Y-m-d H:i:s') midnight" . date('c', strtotime(date('Y-m-d H:i:s
 echo "<br/>2018-12-03: " . date('c', strtotime('2018-12-03'));
 
 echo "<br/><br/>月份";
+echo "<br/>ordinal space dayname space 'of'   first day单用无效";
 echo "<br/>first day of this month: " . date('c', strtotime('first day of this month'));
 echo "<br/>first day of this month  00:00:00  : " . date('c', strtotime('first day of this month 00:00:00'));
 echo "<br/>first day of this month midnight: " . date('c', strtotime('first day of this month midnight'));
 echo "<br/>last day of this month: " . date('c', strtotime('last day of this month midnight'));
+echo "<br/>ordinal space unit";
 echo "<br/>上个月 last month: " . date('c', strtotime('last month'));
+echo "<br/>second month:". date('c', strtotime('second month')), "</br>";
+echo "<br/>twelfth month:". date('c', strtotime('twelfth month')), "</br>";
 echo "<br/>first day of last month: " . date('c', strtotime('first day of last month midnight'));
 echo "<br/>last day of last month: " . date('c', strtotime('last day of last month midnight'));
 
@@ -77,6 +83,7 @@ echo "<br/>weekdays " . date('c', strtotime('weekdays'));
 echo "<br/>-1 weekdays " . date('c', strtotime('-1 weekdays'));
 echo "<br/>-2 weekdays " . date('c', strtotime('-2 weekdays'));
 echo "<br/>-7 weekdays " . date('c', strtotime('-7 weekdays'));
+
 
 
 
