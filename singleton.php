6b0e2e5d9f00df10cb60a73db9c22d05e9d1ff19
siyu <jy1107985549@163.com>
2022-09-20 22:12:09 +0800

    shuffer

--- singleton.php
+++ singleton.php
@@ -4,7 +4,7 @@
 **/
 class Singleton 
 {
-	private static $instance;
+	protected static $instance;
 
 	private function __construct(){}
 
@@ -15,10 +15,47 @@ class Singleton
 		}
 		return self::$instance;
 	}
+
+	/**
+     * 设置当前容器的实例
+     * @access public
+     * @param object|Closure $instance
+     * @return void
+     */
+    public static function setInstance($instance): void
+    {
+        static::$instance = $instance;
+    }
+}
+
+/**
+ * App 基础类
+ */
+class App extends Singleton
+{
+
+	//在单例类的继承类，再次声明实例静态变量，会导致不同的静态变量
+	protected static $instance; 
+    /**
+     * 架构方法
+     * @access public
+     * @param string $rootPath 应用根目录
+     */
+    public function __construct(string $rootPath = '')
+    {
+        static::setInstance($this);
+    }
+
 }
 
 //Fatal error: Uncaught Error: Call to private Singleton::__construct()
 //$instance = new Singleton(); 
 
 
-$instance = Singleton::getInstance();
+//$instance = Singleton::getInstance();
+
+$app = new App();
+
+var_dump(Singleton::getInstance());
+
+
